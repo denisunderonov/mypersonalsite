8ce86caf311984e014b45cce5435cd99840e6c01
denisunderonov <denisunderonov2@gmail.com>
2025-12-09 03:55:01 +0300

    header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,41 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Мой сайт</title>
-    <style>
-        * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-        }
-        body {
-            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, sans-serif;
-            background: #ffffff;
-            color: #333;
-            min-height: 100vh;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-        }
-        .container {
-            text-align: center;
-            padding: 2rem;
-        }
-        h1 {
-            font-size: 2rem;
-            margin-bottom: 1rem;
-            color: #000;
-        }
-        p {
-            font-size: 1.1rem;
-            color: #666;
-        }
-    </style>
+    <title>denisunderonov</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oi&display=swap" rel="stylesheet">
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-    <div class="container">
-        <h1>Привет! 👋</h1>
-        <p>Начните создавать ваш блог-портфолио здесь</p>
-    </div>
+    <x-header />
+    
+    <main>
+        
+    </main>
 </body>
 </html>
