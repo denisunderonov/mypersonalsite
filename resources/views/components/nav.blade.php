7462f67a17e14ab6a80bac3c33f1459ec6f3a3b0
denisunderonov <denisunderonov2@gmail.com>
2026-01-06 06:52:15 +0300

    Добавить отступы по бокам, убрать раздел блог, добавить ссылку на Instagram

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="nav">
-    <a href="/blog" class="nav__link {{ request()->is('blog') ? 'nav__link--active' : '' }}">блог</a>
     <a href="/projects" class="nav__link {{ request()->is('projects') ? 'nav__link--active' : '' }}">проекты</a>
     <a href="https://github.com/denisunderonov" target="_blank" class="nav__link">гитхаб</a>
+    <a href="https://www.instagram.com/d3nisunderonov" target="_blank" class="nav__link">инстаграм</a>
     <a href="https://letterboxd.com/denisunderonov/" target="_blank" class="nav__link">леттербоксэд</a>
     <a href="https://t.me/denisunderonov" target="_blank" class="nav__link">телеграм</a>
     <a href="https://music.apple.com/ru/playlist/hello-its-me/pl.u-kv9l2a5u49pYa7Z" target="_blank" class="nav__link">эпл музыка</a>
