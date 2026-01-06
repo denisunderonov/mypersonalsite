e2e4ee1f88be138bea1c4e6334071b2a89e06904
denisunderonov <denisunderonov2@gmail.com>
2026-01-06 07:29:19 +0300

    Добавить ссылку на YouTube канал в навигацию

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -3,6 +3,7 @@
     <a href="/photos" class="nav__link {{ request()->is('photos') ? 'nav__link--active' : '' }}">фотографии</a>
     <a href="https://github.com/denisunderonov" target="_blank" class="nav__link">гитхаб</a>
     <a href="https://www.instagram.com/d3nisunderonov" target="_blank" class="nav__link">инстаграм</a>
+    <a href="https://www.youtube.com/@denisunderonov.mp4" target="_blank" class="nav__link">ютуб</a>
     <a href="https://letterboxd.com/denisunderonov/" target="_blank" class="nav__link">леттербоксэд</a>
     <a href="https://t.me/denisunderonov" target="_blank" class="nav__link">телеграм</a>
     <a href="https://music.apple.com/ru/playlist/hello-its-me/pl.u-kv9l2a5u49pYa7Z" target="_blank" class="nav__link">эпл музыка</a>
